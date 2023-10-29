001c18c2c5a576a864665c4b858e4feae42dfa84
emi rodriguez <35197166@ies9023.net>
2023-10-29 20:50:36 -0300

    last commit of the app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-const express = require('express');
+/* const express = require('express');
 const morgan = require('morgan');
 const app= express();
 const routes = require('./routes/index');
 
 app.use(morgan('dev'));
-app.use(require('./routes/index'));
+//app.use(require('./routes/index'));
 
+app.use('/',routes);//agregado por corrección
+
+
+module.exports = app; */
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const routes = require('./routes/index');
+
+app.use(morgan('dev'));
+
+// Use the router as middleware
+app.use('/', routes);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
-const express =require('express');
+/* const express =require('express');
 const router = express.Router();
 
 router.get('/',(req,res) => res.json({message:"hello world"}));
 
 module.export = router;
 
+ */
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: "hello world" }));
+
+module.exports = router;
